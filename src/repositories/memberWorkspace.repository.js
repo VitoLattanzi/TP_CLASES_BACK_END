db46418c859cd60c972cb94dbe6e6c0ca6e96b4f
vito lattanzi <vitofrancolattanzi@gmail.com>
2025-10-27 13:23:07 -0300

    arelos

--- src/repositories/memberWorkspace.repository.js
+++ src/repositories/memberWorkspace.repository.js
@@ -58,7 +58,7 @@ class MemberWorkspaceRepository {
     }
 
     static async getAllVyUserId(user_id){
-        const members = MemberWorkspace.find({id_user: user_id})
+        const members = MemberWorkspace.find({id_user: user_id}).populate("id_workspace")
     
         return members
     }
